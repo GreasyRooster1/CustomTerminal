2fa818402d376757df27a69a0a0460ed7dfc1ea8
QuantumDeity <greasyRooster1@gmail.com>
2024-01-27 17:33:49 -0800

    fixed append issues

--- src/Util/Util.java
+++ src/Util/Util.java
@@ -1,11 +1,23 @@
 package Util;
 
+import java.lang.reflect.Array;
+
 public class Util {
-    public static Object[] append(Object[] array1,Object content){
-        Object[] array2 = new Object[]{content};
-        Object[] array = new Object[array1.length + array2.length];
-        System.arraycopy(array1, 0, array, 0, array1.length);
-        System.arraycopy(array2, 0, array, array1.length, array2.length);
+    public static Object expand(Object array) {
+        int len = Array.getLength(array);
+        return expand(array, len > 0 ? len << 1 : 1);
+    }
+
+    public static Object expand(Object list, int newSize) {
+        Class<?> type = list.getClass().getComponentType();
+        Object temp = Array.newInstance(type, newSize);
+        System.arraycopy(list, 0, temp, 0, Math.min(Array.getLength(list), newSize));
+        return temp;
+    }
+    public static Object append(Object array, Object value) {
+        int length = Array.getLength(array);
+        array = expand(array, length + 1);
+        Array.set(array, length, value);
         return array;
     }
 }
